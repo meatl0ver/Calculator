ddf719ffd35a178396882b914ce9982f1900582d
meatl0ver <raulimashev@yandex.ru>
2021-04-26 23:28:43 +0300

    clean directory

--- app/src/main/java/raui/imashev/homework/MainActivity.java
+++ app/src/main/java/raui/imashev/homework/MainActivity.java
@@ -2,19 +2,9 @@ package raui.imashev.homework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
-
-import java.util.ArrayList;
-
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 
 public class MainActivity extends AppCompatActivity{
 
